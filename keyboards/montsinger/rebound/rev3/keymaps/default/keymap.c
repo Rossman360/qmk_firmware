129b65e4912e1d2a8727f4fe972b0d4d86ce7be8
Rossman360 <rmontsinger@gmail.com>
2020-05-23 10:28:22 -0400

    filled out default keymap some more

--- keyboards/montsinger/rebound/rev3/keymaps/default/keymap.c
+++ keyboards/montsinger/rebound/rev3/keymaps/default/keymap.c
@@ -27,10 +27,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_all(
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_1,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_2,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_3,    KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+   KC_1,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+   KC_2,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_A,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+   KC_3,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_SPC, KC_C,    KC_SPC,  KC_9,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_COLEMAK] = LAYOUT_all(
