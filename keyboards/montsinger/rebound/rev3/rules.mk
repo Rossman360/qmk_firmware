c62cdf58c4766d04a5db378e1dac1bfbcef51950
Rossman360 <rmontsinger@gmail.com>
2020-05-28 12:16:44 -0400

    test

--- keyboards/montsinger/rebound/rev3/rules.mk
+++ keyboards/montsinger/rebound/rev3/rules.mk
@@ -29,6 +29,6 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 ENCODER_ENABLE = yes
 
-LAYOUTS = ortho_4x12
+LAYOUTS = ortho_4x12,
 LAYOUTS_HAS_RGB = no
 
