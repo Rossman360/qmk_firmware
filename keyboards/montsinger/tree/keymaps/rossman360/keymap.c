064ce482230fa8a97b8e51b54e9cda1965f23f6f
Rossman360 <rmontsinger@gmail.com>
2021-03-19 19:55:19 -0400

    now with named layers 'cause it's easier

--- keyboards/montsinger/tree/keymaps/rossman360/keymap.c
+++ keyboards/montsinger/tree/keymaps/rossman360/keymap.c
@@ -16,37 +16,50 @@
 #include QMK_KEYBOARD_H 
 #include "rossman360.h"
 
+#define PGMOD LT(_NUM, KC_PGDN)
+#define TABMOD LT(_FN1, KC_TAB)
+#define SPCMOD LT(_FN1, KC_SPACE)
+#define ENTMOD LT(_FN2, KC_ENTER)
+#define ESCMOD LT(_NUM, KC_ESC)
+#define RSMOD LT(_FN1, KC_RSHIFT)
 
 enum encoder_names {
   _LEFT,
   _RIGHT,
 };
 
+enum layer_names {
+  _BASE,
+  _DEL,
+  _FN1,
+  _FN2,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_all(
+    [_BASE] = LAYOUT_all(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         JUMPBACK,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_CAPS, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, KC_UP, KC_R,
-        CMDBSP,  ALTDEL,  KC_LCTL, KC_LCTL, KC_TAB,  KC_PGDN, KC_ENT,  KC_SPC,  MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+        CMDBSP,  ALTDEL,  KC_LCTL, KC_LCTL, KC_TAB,  KC_PGDN, KC_ENT,  SPCMOD,  MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
-    [1] = LAYOUT_all(
+    [_FN1] = LAYOUT_all(
         RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, BLINE,   KC_BSPC, BWORD,   XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
+        _______, _______, _______, BLINE,   LWORD,   RWORD,   XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
     ),
     
-    [2] = LAYOUT_all(
+    [_FN2] = LAYOUT_all(
         RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, BLINE,   KC_BSPC, BWORD,   XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
     ),
-    [3] = LAYOUT_all(
+    [_DEL] = LAYOUT_all(
         RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
