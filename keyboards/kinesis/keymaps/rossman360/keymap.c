11864b305e3f83797339362c48687a5ddc80d855
Ross Montsinger <ross@Rosss-MacBook-Air.local>
2021-08-02 15:46:32 -0400

    desktop macros

--- keyboards/kinesis/keymaps/rossman360/keymap.c
+++ keyboards/kinesis/keymaps/rossman360/keymap.c
@@ -7,7 +7,10 @@
 #define ENTMOD LT(_FN2, KC_ENTER)
 #define ESCMOD LT(_NUM, KC_ESC)
 #define RSMOD LT(_FN1, KC_RSHIFT)
-#
+#define DESK1 LCMD(KC_1)
+#define DESK2 LCMD(KC_2)
+#define DESK3 LCMD(KC_3)
+
 
 enum layer_names {
   _BASE,
@@ -22,14 +25,14 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_pretty(
-    KC_GRAVE, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    PMERGE,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_FN0,   RESET,
+    KC_GRAVE, DESK1,    DESK2,    DESK3,    KC_F4,    KC_F5,    KC_F6,    XPANDR,    PMERGE,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_FN0,   RESET,
     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     NTAB,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     JUMPBACK, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS,
               CMDBSP,   ALTDEL,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
-                                                      CTRLSP,   KC_LALT,                                               KC_VOLU,  MO(_DEL),
-                                                                KC_HOME,                                               KC_VOLD,
+                                                      CTRLSP,   KC_F4,                                                 KC_VOLU,  MO(_DEL),
+                                                                KC_F3,                                                 KC_VOLD,
                                             KC_TAB,   PGMOD,    KC_ENTER,                                              KC_BSPC,  ENTMOD,   SPCMOD
   ),
  [_FN1] = LAYOUT_pretty(
