0fabf280f234f4cd6d509f980a6255f712501a26
Ross Montsinger <ross@Rosss-MacBook-Air.local>
2021-08-05 15:08:35 -0400

    caps

--- keyboards/kinesis/keymaps/rossman360/keymap.c
+++ keyboards/kinesis/keymaps/rossman360/keymap.c
@@ -21,7 +21,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_pretty(
-    KC_GRAVE, DESK1, DESK2, DESK3, EXPOSE, KC_F5, KC_F6, XPANDR, PMERGE,                                         KC_COLN, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_FN0, RESET,
+    KC_GRAVE, DESK1, DESK2, DESK3, EXPOSE, KC_F5, KC_F6, XPANDR, PMERGE,                                         KC_CAPS, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_FN0, RESET,
     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     NTAB,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     JUMPBACK, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
