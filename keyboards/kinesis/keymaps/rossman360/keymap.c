b7584e0979aac4daf89d688bcd563f800b7f1ea7
Ross Montsinger <ross@Rosss-MacBook-Air.local>
2021-08-11 11:38:58 -0400

    ctab

--- keyboards/kinesis/keymaps/rossman360/keymap.c
+++ keyboards/kinesis/keymaps/rossman360/keymap.c
@@ -8,6 +8,7 @@
 #define DESK1 LCMD(KC_1)
 #define DESK2 LCMD(KC_2)
 #define DESK3 LCMD(KC_3)
+#define DESK4 LCMD(KC_3)
 #define EXPOSE LCMD(KC_UP)
 
 enum layer_names {
@@ -21,9 +22,9 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_pretty(
-    KC_GRAVE, DESK1, DESK2, DESK3, EXPOSE, KC_F5, KC_F6, XPANDR, PMERGE,            KC_CAPS, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_FN0, RESET,
+    EXPOSE,   DESK1, DESK2, DESK3, DESK4, KC_F5, KC_F6, XPANDR, PMERGE,            KC_CAPS, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_FN0, RESET,
     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     NTAB,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+    TABMOD,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     CTAB,
     JUMPBACK, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                            KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS,
               CMDBSP,   ALTDEL,   KC_LEFT,  KC_RGHT,                                                             KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_FN1] = LAYOUT_pretty(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    _______,  _______,  _______,  _______,  _______,  _______,                                        _______,  _______,  _______,  _______,  _______,  CTAB,
+    KC_GRAVE, _______,  _______,  _______,  _______,  _______,                                        _______,  _______,  _______,  _______,  _______,  CTAB,
     _______,  _______,  _______,  _______,  _______,  _______,                                        _______,  UNDO,     _______,  _______,  _______,  _______,
     KC_CAPS,  _______,  _______,  _______,  _______,  _______,                                        KC_HOME,  KC_LEFT,  KC_UP,    KC_RIGHT, KC_END,   _______,
     _______,  _______,  _______,  _______,  _______,  _______,                                        _______,  PMERGE,   KC_DOWN,  _______,  _______,  _______,
