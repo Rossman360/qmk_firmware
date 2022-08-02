03c58ea2907ebbf037bb8879c204b43e3dfca005
Ross <rmontsinger@gmail.com>
2022-08-02 10:05:30 -0400

    remcaps

--- users/rossman360/rossman360.c
+++ users/rossman360/rossman360.c
@@ -72,7 +72,7 @@ switch (keycode) {
 	break;
 	case REMCAPS:
 	if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_LALT(SS_TAP(X_LEFT)) SS_TAP(X_DELETE));
+		SEND_STRING(SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_LCTRL(SS_TAP(X_LEFT)) SS_TAP(X_DELETE));
 	}
 	break;
 	case EMDASH:
