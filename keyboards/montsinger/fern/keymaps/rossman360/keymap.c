3c31e9c0ea821cc0c286fb17d678ac4b0238399b
Ross <rmontsinger@gmail.com>
2023-07-27 13:17:09 -0400

    screen off stuff

--- keyboards/montsinger/fern/keymaps/rossman360/keymap.c
+++ keyboards/montsinger/fern/keymaps/rossman360/keymap.c
@@ -23,16 +23,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   SOFF,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+    KC_F13,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   QK_BOOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
     KC_BSPC, ALTDEL,           CTRLSP,  KC_TAB,  PGMOD,         KC_ENT,  SPCMOD,  MO(_DEL),         KC_BSPC, KC_DEL
 ),
 
 [_FN1] = LAYOUT(
       _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, 
      _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,        KC_HOME, KC_LEFT, KC_UP,   KC_RIGHT,KC_END,  _______,
-   _______, _______, _______, _______, _______, _______, QK_BOOT, _______, _______, KC_DOWN, _______, _______, _______,
+    KC_CAPS, _______, _______, _______, _______, _______,        KC_HOME, KC_LEFT, KC_UP,   KC_RIGHT,KC_END,  _______,
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, _______, _______,
     _______, _______,          _______, LWORD,   RWORD,          _______, _______, _______,           _______, _______
 ),
 
